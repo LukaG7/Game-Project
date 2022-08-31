a304b2d9fb7eb1df830e9b21f99f2090a9a27227
Lukas Gonzalez <lukegonz47@gmail.com>
2022-08-31 14:12:56 -0400

    modified some html and js

---
+++ index.js
@@ -0,0 +1,7 @@
+const paddle1 = document.getElementById("paddle1");
+const ball = document.getElementById("ball");
+const paddle2 = document.getElementById("paddle2");
+
+paddle1 === paddle2
+
+console.log(paddle1)
